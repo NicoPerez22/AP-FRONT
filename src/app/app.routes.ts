193eedd0d2a7ca804dab7da58c221a2d1323ca36
Ezequiel Merino Spinedi <merino.eze@gmail.com>
2025-04-15 19:33:25 -0300

    Adds login route title
    Integrates a title metadata property into the login route to improve UI clarity.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { LogoutComponent } from './protected/logout/logout.component';
 export const routes: Routes = [
   {
     path: 'login',
+    title: 'Inicio de sesión',
     component: LoginComponent,
     canActivate: [LoginGuard],
   },
