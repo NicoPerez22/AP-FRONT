e55df6107ec8e16dec4571eeae1c22afbc1f9470
Ezequiel Merino Spinedi <merino.eze@gmail.com>
2025-04-24 20:51:04 -0300

    Update login form validation to require a valid email format for username

--- src/app/protected/login/login.component.ts
+++ src/app/protected/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      username: ['', [Validators.required]],
+      username: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
   }
